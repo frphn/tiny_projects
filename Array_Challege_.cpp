5b1c3902fb019854b1507c2108be7e913e0ffca4
user <fpushan@gmail.com>
2020-07-03 22:07:24 -0400

    random projects
    for coding fun

---
+++ Array_Challege_.cpp
@@ -0,0 +1,24 @@
+/*
+Array Challenge Solution
+
+The following program will
+ first fill in (by asking the user for input)
+  and then list all the elements in an array:
+
+*/
+
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int array[8];
+    cout<< "Enter Input: ";
+    for (int x = 0; x<8; x++)
+    cin>> array[8];
+     //array[ x ] = i + 100;
+    for(int x = 0; x<8; x++)
+    cout<<  array[8] << endl;
+    return 0;
+}
