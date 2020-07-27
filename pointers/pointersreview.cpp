84c50d46040ddebe61ef871177f5097bdf058cb3
user <fpushan@gmail.com>
2020-07-27 15:07:58 -0400

    I was reviewing

---
+++ pointers/pointersreview.cpp
@@ -0,0 +1,11 @@
+//the main goal of this program is
+//to print out a recollection of what
+//pointers are
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<< "Pointers\n are variables that point to another variable that stores a memory address" << endl;
+}
